60ab138fd6bf9edcc8fb0d4d419400980f0140fa
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-10-18 08:21:25 -0600

    Supplemental Order.py

--- Supplemental Order.py
+++ Supplemental Order.py
@@ -1,3 +1,10 @@
+# Steps
+# 1. Download Supplemental Order data from DSS and name in Supplemental Order Data
+# 2. Download Available Inventory as a CSV from DOMO and name in "Available Inventory"
+# 3. Run script and enjoy the results
+# 4. Copy data from sto_single_ file into NOVA sheet
+
+#importing required libraries
 import pandas as pd
 import numpy as np
 import openpyxl
@@ -5,8 +12,9 @@ from openpyxl import load_workbook
 from openpyxl.formula import Tokenizer
 from openpyxl.formula.translate import Translator
 
-df = pd.read_excel(r"C:\Users\clata\OneDrive\Desktop\python\work\supplemental order\Supplemental Order Data.xlsx", skiprows=24)
-available_inventory = pd.read_csv(r"C:\Users\clata\OneDrive\Desktop\python\work\supplemental order\Available Inventory.csv")
+# import necessary files
+df = pd.read_excel(r"Supplemental Order Data.xlsx", skiprows=24)
+available_inventory = pd.read_csv(r"Available Inventory.csv")
 
 # getting the unique list of SKU's and saving it to dataframe
 unique_sku = df['Vendor Stk Nbr'].unique()
@@ -14,24 +22,24 @@ unique_sku = df['Vendor Stk Nbr'].unique()
 # creating units needed calculation
 df['6_wk_fcst'] = df.iloc[:,16:22].sum(axis=1)
 df['pipe_minus_fcst'] = df['PIPELINE'] - df['6_wk_fcst']
+df['pipe_minus_fcst'] = df.apply(lambda row: 0 if row['pipe_minus_fcst'] > 0 else row['pipe_minus_fcst'], axis=1)
+df['pipe_minus_fcst'] = df['pipe_minus_fcst'].abs()
 df['whse_pks_needed'] = df['pipe_minus_fcst'] / df['VNPK Qty']
-
-df['units_needed'] = df.apply(lambda row: 0 if row['whse_pks_needed'] > 0 else row['whse_pks_needed'] * row['VNPK Qty'], axis=1)
-df['needed_vnpk_qty'] = df.apply(lambda row: 3 * row['VNPK Qty'] if row["VNPK Qty"] == 1 else row['VNPK Qty'] * 1, axis=1)
-df['pipe_need'] = df.apply(lambda row: row['needed_vnpk_qty'] if row['pipe_minus_fcst'] < 0 else row['units_needed'], axis=1)
-
+df['whse_pks_needed'] = df['whse_pks_needed'].apply(np.ceil)
+df['pipe_need'] = df['whse_pks_needed'] * df['VNPK Qty']
+df['pipe_need'] = df.apply(lambda row: row['Max Shelf Qty'] if row['pipe_need'] > row['Max Shelf Qty'] else row['pipe_need'], axis=1)
 
 sto_single = pd.DataFrame()
 
 for item in unique_sku:
     df_filtered = df[df['Vendor Stk Nbr'] == item]
-    df_filtered = df_filtered.sort_values('PIPELINE', ascending=True).reset_index()
+    df_filtered = df_filtered[df_filtered['Curr Valid Store/Item Comb.'] == 1]
+    df_filtered = df_filtered[df_filtered['Store Type Descr'] != 'BASE STR Nghbrhd Mkt']
+    df_filtered = df_filtered.sort_values('pipe_need', ascending=False).reset_index()
 
     # getting available inventory 
-    inv_item = item
-    item_string = str(inv_item)
-    available_inventory[available_inventory['Item'] == item_string]
-    available_inv = available_inventory.iloc[0][2]
+    blkst_oh = available_inventory[available_inventory['Item'] == str(item)]
+    available_inv = blkst_oh.iloc[0][2]
 
     #reducing available inventory for shared items
     shared_items = [1528,4114,5017,5091,5249,5471]
@@ -71,6 +79,6 @@ for item in unique_sku:
     # dropping rows where BLKST isn't sending anything
     sto_single = sto_single.loc[sto_single['units_sent_dc'] != 0]
 
-
-df.to_excel(r"C:\Users\clata\OneDrive\Desktop\python\work\supplemental order\Supplemental Order TESTER.xlsx")
-sto_single.to_excel(r"C:\Users\clata\OneDrive\Desktop\python\work\supplemental order\sto_single_.xlsx")
+# exporting data to new files
+df_filtered.to_excel(r"Supplemental Order TESTER.xlsx")
+sto_single.to_excel(r"sto_single_.xlsx")
