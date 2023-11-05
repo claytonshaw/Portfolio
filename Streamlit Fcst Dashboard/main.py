774154e7e53e30544081ed05612ac3af994ae8db
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-11-05 00:27:37 -0600

    Update main.py

--- Streamlit Fcst Dashboard/main.py
+++ Streamlit Fcst Dashboard/main.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 # Title
 st.title("SOF Dashboard")
 
-# Load data from the CSV file
+# LOADING DATA
 @st.cache_data
 def load_data():
     data = pd.read_csv("Forecast vs SO Quantity Table.csv")
@@ -20,11 +20,23 @@ def load_data():
 
 df = load_data()
 
+# DATES AND RANGES
+current_year = date.today().year
+next_year = current_year + 1
+last_year = current_year - 1
+today = pd.to_datetime(date.today())
+today_last_year = today.replace(year=today.year - 1)
+start_of_year = pd.to_datetime(date(today.year, 1, 1))
+start_of_last_year = pd.to_datetime(date(today_last_year.year, 1, 1))
+
 # Extract month and year from 'Ship Date'
 df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 df['Year'] = df['Ship Date'].dt.year
 df['Month'] = df['Ship Date'].dt.month
 
+
+
+# SIDEBAR
 st.sidebar.header("Filters")
 selected_account_manager = st.sidebar.selectbox("Select Account Manager", ["All"] + list(sorted(df['Account Manager'].unique())))
 
@@ -60,63 +72,71 @@ if selected_parent != "All":
 if selected_item != "All":
     filtered_data = filtered_data[filtered_data['Item'] == selected_item]
 
-# Calculate total SO Quantity and Total Forecast Qty
-total_so_quantity = int(filtered_data['SO Quantity'].sum())
-total_forecast_qty = int(filtered_data['Forecast Qty'].sum())
 
-# Format total numbers with commas
-total_so_quantity_formatted = '{:,}'.format(total_so_quantity)
-total_forecast_qty_formatted = '{:,}'.format(total_forecast_qty)
 
-# Calculate the forecast variance as (Total Forecast Qty - Total SO Quantity) / Total SO Quantity
-forecast_variance = (total_forecast_qty - total_so_quantity) / total_so_quantity
+# CALCULATIONS FOR KPI'S
+# YTD Sales Orders and Forecasts
+total_so_qty = int(filtered_data.loc[(filtered_data['Ship Date'] > start_of_year) & (filtered_data['Ship Date'] < today), 'SO Quantity'].sum())
+total_forecast_qty = int(filtered_data.loc[(filtered_data['Ship Date'] > start_of_year) & (filtered_data['Ship Date'] < today), 'Forecast Qty'].sum())
+total_so_qty_formatted = '{:,}'.format(total_so_qty) # format with commas
+total_forecast_qty_formatted = '{:,}'.format(total_forecast_qty) # format with commas
 
-# Format forecast variance as a percentage
-forecast_variance_formatted = f'{forecast_variance:.2%}'
+# Forecast Variance
+forecast_variance = (total_forecast_qty - total_so_qty) / total_so_qty
+forecast_variance_formatted = f'{forecast_variance:.2%}' # format as a percentage
 
-# Year Variables
-current_year = date.today().year
-next_year = current_year + 1
-last_year = current_year - 1
+# Calculate SO Quantity growth from this year to last year
+last_year_so_qty = int(filtered_data.loc[(filtered_data['Ship Date'] > start_of_last_year) & (filtered_data['Ship Date'] < today_last_year), 'SO Quantity'].sum())
+so_qty_growth = (total_so_qty - last_year_so_qty) / last_year_so_qty
+so_qty_growth_formatted = f'{so_qty_growth: .2%}' # format as  a percentage
 
 # Creating Prophet Forecast
 next_year_prophet = forecaster(filtered_data,next_year)
 
+
+
+# CREATING DATAFRAMES 
 # Create a new DataFrame for next year's forecast
-next_year_forecast = filtered_data[filtered_data['Year'] == next_year].groupby(['Year', 'Month']).agg({'Forecast Qty': 'sum'}).reset_index()
-next_year_forecast['SO Quantity'] = 0  # Set SO Quantity to 0 for next year forecast
-next_year_forecast.rename(columns = {'Forecast Qty':'Next Year Forecast'}, inplace = True)
+next_year_forecast_df = filtered_data[filtered_data['Year'] == next_year].groupby(['Year', 'Month']).agg({'Forecast Qty': 'sum'}).reset_index()
+next_year_forecast_df['SO Quantity'] = 0  # Set SO Quantity to 0 for next year forecast
+next_year_forecast_df.rename(columns = {'Forecast Qty':'Next Year Forecast'}, inplace = True)
 
 # Create a new DataFrame for this year's forecast
-this_year_forecast = filtered_data[filtered_data['Year'] == current_year].groupby(['Year', 'Month']).agg({'Forecast Qty': 'sum'}).reset_index()
-this_year_forecast['SO Quantity'] = 0  # Set SO Quantity to 0 for this year forecast
-this_year_forecast.rename(columns = {'Forecast Qty': 'This Year Forecast'}, inplace = True)
+this_year_forecast_df = filtered_data[filtered_data['Year'] == current_year].groupby(['Year', 'Month']).agg({'Forecast Qty': 'sum'}).reset_index()
+this_year_forecast_df['SO Quantity'] = 0  # Set SO Quantity to 0 for this year forecast
+this_year_forecast_df.rename(columns = {'Forecast Qty': 'This Year Forecast'}, inplace = True)
 
 # Create a new DataFrame for this year's SO Quantity
-this_year_so_quantity = filtered_data[filtered_data['Year'] == current_year].groupby(['Year','Month']).agg({'SO Quantity': 'sum'}).reset_index()
-this_year_so_quantity.rename(columns = {'SO Quantity': 'This Year SO Quantity'}, inplace = True)
+this_year_so_qty_df = filtered_data[filtered_data['Year'] == current_year].groupby(['Year','Month']).agg({'SO Quantity': 'sum'}).reset_index()
+this_year_so_qty_df['Forecast Qty'] = 0
+this_year_so_qty_df.rename(columns = {'SO Quantity': 'This Year SO Quantity'}, inplace = True)
 
 # Create a new Data Frame for last year's SO Quantity
-last_year_so_quantity = filtered_data[filtered_data['Year'] == last_year].groupby(['Year','Month']).agg({'SO Quantity':'sum'}).reset_index()
-last_year_so_quantity.rename(columns = {'SO Quantity': 'Last Year SO Quantity'}, inplace = True)
+last_year_so_qty_df = filtered_data[filtered_data['Year'] == last_year].groupby(['Year','Month']).agg({'SO Quantity':'sum'}).reset_index()
+last_year_so_qty_df['Forecast Qty'] = 0
+last_year_so_qty_df.rename(columns = {'SO Quantity': 'Last Year SO Quantity'}, inplace = True)
 
 # Concatenate the DataFrames for current year, next year, and previous years
 combined_df = pd.DataFrame()
-combined_df['Month'] = this_year_forecast['Month']
-combined_df['This Year SO Quantity'] = this_year_so_quantity['This Year SO Quantity'].astype(int)
-combined_df['Last Year SO Quantity'] = last_year_so_quantity['Last Year SO Quantity'].astype(int)
-combined_df['This Year Forecast'] = this_year_forecast['This Year Forecast'].astype(int)
-combined_df['Next Year Forecast'] = next_year_forecast['Next Year Forecast'].astype(int)
+combined_df['Month'] = this_year_forecast_df['Month']
+combined_df['This Year SO Quantity'] = this_year_so_qty_df['This Year SO Quantity'].astype(int)
+combined_df['Last Year SO Quantity'] = last_year_so_qty_df['Last Year SO Quantity'].astype(int)
+combined_df['This Year Forecast'] = this_year_forecast_df['This Year Forecast'].astype(int)
+combined_df['Next Year Forecast'] = next_year_forecast_df['Next Year Forecast'].astype(int)
+
+
+
+# ADDING ELEMENTS TO WEBPAGE
+left_column, right_column = st.columns([3,1])   
+
+# create an editable dataframe
+combined_df.drop(columns = ['This Year SO Quantity','Last Year SO Quantity','This Year Forecast'], inplace = True)
+combined_df_editable = left_column.data_editor(combined_df.T)
+combined_df_reposed = combined_df_editable.T.reset_index()
 
 # Calculate Next Year Total Forecast Qty
-next_year_total_forecast = int(next_year_forecast['Next Year Forecast'].sum())
+next_year_total_forecast = int(combined_df_reposed['Next Year Forecast'].sum())
 next_year_total_forecast_formatted = '{:,}'.format(next_year_total_forecast)
-
-# Calculate SO Quantity growth from this year to last year
-this_year_so_quantity_formatted = this_year_so_quantity['This Year SO Quantity'].sum()
-last_year_so_quantity_formatted = last_year_so_quantity['Last Year SO Quantity'].sum()
-so_quantity_growth = (this_year_so_quantity_formatted - last_year_so_quantity_formatted) / last_year_so_quantity_formatted
-so_quantity_growth_formatted = f'{so_quantity_growth: .2%}'
 
 # Apply CSS for styling the total boxes
 st.markdown(
@@ -140,37 +160,32 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Display the total boxes with values and labels
-total_col1, total_col2, total_col3, total_col4, total_col5 = st.columns(5)
-total_col1.markdown(f'<div class="total-box"><div class="total-label">Total SO Quantity</div>{total_so_quantity_formatted}</div>', unsafe_allow_html=True)
-total_col2.markdown(f'<div class="total-box"><div class="total-label">This Year Total Forecast</div>{total_forecast_qty_formatted}</div>', unsafe_allow_html=True)
-total_col3.markdown(f'<div class="total-box"><div class="total-label">Forecast Variance</div>{forecast_variance_formatted}</div>', unsafe_allow_html=True)
-total_col4.markdown(f'<div class="total-box"><div class="total-label">Next Year Total Forecast</div>{next_year_total_forecast_formatted}</div>', unsafe_allow_html=True)
-total_col5.markdown(f'<div class="total-box"><div class="total-label">SO Quantity Growth</div>{so_quantity_growth_formatted}</div>', unsafe_allow_html=True)
+# Instruction Box
+new_forecast = right_column.write('Instructions: Select from filters and then try typing a new number in the table.')
 
 st.divider()
 
-left_column, right_column = st.columns(2)   
-
-# create an editable dataframe
-combined_df.drop(columns = ['This Year SO Quantity','Last Year SO Quantity'], inplace = True)
-combined_df_editable = left_column.data_editor(combined_df.T)
-ombined_df_reposed = combined_df_editable.T.reset_index()
-
-new_forecast = right_column.write('Instructions:Try typing a new number in the Next Years Forecast Grid and watch the graph change.')
+# Display the total boxes with values and labels
+total_col1, total_col2, total_col3, total_col4, total_col5 = st.columns(5)
+total_col1.markdown(f'<div class="total-box"><div class="total-label">SO Quantity</div>{total_so_qty_formatted}</div>', unsafe_allow_html=True)
+total_col2.markdown(f'<div class="total-box"><div class="total-label">SO Quantity Growth</div>{so_qty_growth_formatted}</div>', unsafe_allow_html=True)
+total_col3.markdown(f'<div class="total-box"><div class="total-label">Forecast Variance</div>{forecast_variance_formatted}</div>', unsafe_allow_html=True)
+total_col4.markdown(f'<div class="total-box"><div class="total-label">Total Forecast</div>{total_forecast_qty_formatted}</div>', unsafe_allow_html=True)
+total_col5.markdown(f'<div class="total-box"><div class="total-label">Next Year Total Forecast</div>{next_year_total_forecast_formatted}</div>', unsafe_allow_html=True)
 
 # Create the line graph figure
 fig_updated = px.line(
-    ombined_df_reposed,
+    combined_df_reposed,
     x='Month',
-    y=['This Year Forecast','Next Year Forecast'],
-    color_discrete_map={'This Year Forecast':'#cc5628', 'Next Year Forecast':'red'}
+    y=['Next Year Forecast'],
+    color_discrete_map={'Next Year Forecast':'red'}
 )
 
 # Add the lines for "This Year Forecast" and "Next Year Forecast"
-fig_updated.add_scatter(x=this_year_so_quantity['Month'], y=this_year_so_quantity['This Year SO Quantity'], mode='lines', name='This Year SO Quantity', line=dict(color='#2f4359'))
-fig_updated.add_scatter(x=this_year_so_quantity['Month'], y=last_year_so_quantity['Last Year SO Quantity'], mode='lines', name='Last Year SO Quantity', line=dict(color='#949598'))
-fig_updated.add_scatter(x=next_year_prophet['Month'], y=next_year_prophet['yhat'], mode='lines', name='Prophet', line=dict(color='pink'))
+fig_updated.add_scatter(x=this_year_forecast_df['Month'], y=this_year_forecast_df['This Year Forecast'], mode='lines', name='This Year Forecast', line=dict(color='#cc5628'))
+fig_updated.add_scatter(x=this_year_so_qty_df['Month'], y=this_year_so_qty_df['This Year SO Quantity'], mode='lines', name='This Year SO Quantity', line=dict(color='#2f4359'))
+fig_updated.add_scatter(x=last_year_so_qty_df['Month'], y=last_year_so_qty_df['Last Year SO Quantity'], mode='lines', name='Last Year SO Quantity', line=dict(color='#949598'))
+fig_updated.add_scatter(x=next_year_prophet['Month'], y=next_year_prophet['yhat'], mode='lines', name='Prophet Forecast', line=dict(color='pink'))
 
 fig_updated.update_xaxes(title_text="Month Number", showgrid=True, dtick=True)
 fig_updated.update_yaxes(title_text="Unit Quantity")
