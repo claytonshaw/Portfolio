4f32902e6dbc694f94891d926afed50f669b417a
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-11-18 13:25:55 -0700

    Update stock_breakout_df.py

--- Stock Breakout/stock_breakout_df.py
+++ Stock Breakout/stock_breakout_df.py
@@ -1,11 +1,37 @@
 from functions import breakout_profitability
+from yahoo_fin import stock_info as si
+import pandas as pd
+import numpy as np
 
-sp500_top_stocks = [
-    'TSLA',
-    'AMZN',
-    'ZM',
-    'NIO',
-    'PLTR'
-]
-for i in sp500_top_stocks:
-    breakout_profitability(i)
+# gather stock symbols from major US exchanges
+df1 = pd.DataFrame(si.tickers_sp500())
+df2 = pd.DataFrame(si.tickers_nasdaq())
+df3 = pd.DataFrame(si.tickers_dow())
+df4 = pd.DataFrame(si.tickers_other())
+
+# convert DataFrame to list, then to sets
+sym1 = set( symbol for symbol in df1[0].values.tolist() )
+sym2 = set( symbol for symbol in df2[0].values.tolist() )
+sym3 = set( symbol for symbol in df3[0].values.tolist() )
+sym4 = set( symbol for symbol in df4[0].values.tolist() )
+
+# join the 4 sets into one. Because it's a set, there will be no duplicate symbols
+symbols = set.union( sym1)
+
+# Some stocks are 5 characters. Those stocks with the suffixes listed below are not of interest.
+my_list = ['W', 'R', 'P', 'Q']
+del_set = set()
+sav_set = set()
+
+for symbol in symbols:
+    if len( symbol ) > 4 and symbol[-1] in my_list:
+        del_set.add( symbol )
+    else:
+        sav_set.add( symbol )
+
+print( f'Removed {len( del_set )} unqualified stock symbols...' )
+print( f'There are {len( sav_set )} qualified stock symbols...' )
+
+
+for i in sav_set:
+    breakout_profitability(i, dollars = 1000)
