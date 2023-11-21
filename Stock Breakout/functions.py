052d44c297d652d2b15b8ef286839ef8aebb70fa
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-11-21 07:55:29 -0700

    Update functions.py

--- Stock Breakout/functions.py
+++ Stock Breakout/functions.py
@@ -3,56 +3,54 @@ import numpy as np
 
 def breakout_profitability(ticker, dollars):
     '''A function that returns a histogram and probabilistic information for all breakouts of a stock using its historical daily prices'''
+    
     # Import libraries
     from yahoo_fin.stock_info import get_data
     import pandas as pd
     import numpy as np
     import seaborn as sns
     import matplotlib.pyplot as plt
+    from datetime import datetime
+    
     ## PREPARE OUR DATAFRAME FOR ANALYSIS
     try:
-    # Get the historical weekly prices from the specified start date and end date (both YYYY-mm-dd)
+        # Get the historical weekly prices from the specified start date and end date (both YYYY-mm-dd)
         hist = get_data(ticker, index_as_date=False)
-        # Drop the adjusted close column
-        prices = hist.drop(['adjclose'], axis=1)
-        # Get the selling pressure, which is distance between high and close
-        prices['SellingPressure'] = prices['high'] - prices['close']
-        # Get the length of candle's body (from open to close)
-        prices['O-to-C'] = prices['close'] - prices['open'] 
-        # Get the rolling mean of the candle's body for recent 20 candles
-        prices['OC-20D-Mean'] = prices['O-to-C'].rolling(20).mean()
-        # Get the % increase or decrease of the current body length from the rolling mean
-        prices['OC-%-from-20D-Mean'] = 100*(prices['O-to-C'] - prices['OC-20D-Mean'])/prices['OC-20D-Mean']
-        # Get the maximum OC compared to the recent 10 candles
-        prices['MaxOC_Prev10'] = prices['O-to-C'].rolling(10).max()
-        # Get the rolling mean of volume for the recent 20 candles
-        prices['Volume-20D-Mean'] = prices['volume'].rolling(20).mean()
-        # Get the % increase or decrease of the current volume from the rolling mean
-        prices['Volume-%-from-20D-Mean'] = 100*(prices['volume'] - prices['Volume-20D-Mean'])/prices['Volume-20D-Mean']
-        # Drop the null values for the first 19 rows, where no mean can be computed yet.
-        prices = prices.dropna()
-        # Rearrange columns
-        prices = prices[['ticker', 'date', 'open', 'high', 'low', 'close', 
-                        'O-to-C', 'OC-20D-Mean', 'volume', 'Volume-20D-Mean', 
-                        'MaxOC_Prev10', 'SellingPressure', 'OC-%-from-20D-Mean', 
-                        'Volume-%-from-20D-Mean', 
-                        ]]
-    except (KeyError, AssertionError):
+    except AssertionError:
         pass
-        # Select the subset of dataframe where breakout conditions apply
-        # Conditions: 
-        # 1. green candle, 
-        # 2. candle's body is longest in 10 days, 
-        # 3. breakout volume is 50% higher than the rolling 20-day average, and
-        # 4. breakout candle has body that is 100% higher than the rolling 20-day average
-    try:
-        condition = (prices['O-to-C'] > 0.0) & (prices['O-to-C'] == prices['MaxOC_Prev10']) & (prices['OC-%-from-20D-Mean'] >= 100.0) & (prices['SellingPressure']/prices['O-to-C'] <= 0.40) & (prices['Volume-%-from-20D-Mean'] >= 50.0) 
-        breakouts = prices[condition]
-        ## GET THE PROFIT (OR LOSS) PER CANDLE
-        # Get the index (from the dataframe) of each breakout row, which is necessary for looping later
-        breakouts_indices = breakouts.index.tolist()
-    except UnboundLocalError:
-        pass
+    # Drop the adjusted close column
+    prices = hist.drop(['adjclose'], axis=1)
+    # Get the selling pressure, which is distance between high and close
+    prices['SellingPressure'] = prices['high'] - prices['close'] 
+    # Get the length of candle's body (from open to close)
+    prices['O-to-C'] = prices['close'] - prices['open']
+    # Get the rolling mean of the candle's body for recent 20 candles
+    prices['OC-20D-Mean'] = prices['O-to-C'].rolling(20).mean()
+    # Get the % increase or decrease of the current body length from the rolling mean
+    prices['OC-%-from-20D-Mean'] = 100*(prices['O-to-C'] - prices['OC-20D-Mean'])/prices['OC-20D-Mean']
+    # Get the maximum OC compared to the recent 10 candles
+    prices['MaxOC_Prev10'] = prices['O-to-C'].rolling(10).max()
+    # Get the rolling mean of volume for the recent 20 candles
+    prices['Volume-20D-Mean'] = prices['volume'].rolling(20).mean()
+    # Get the % increase or decrease of the current volume from the rolling mean
+    prices['Volume-%-from-20D-Mean'] = 100*(prices['volume'] - prices['Volume-20D-Mean'])/prices['Volume-20D-Mean']
+    # Drop the null values for the first 19 rows, where no mean can be computed yet.
+    prices = prices.dropna()
+    # Rearrange columns
+    prices = prices[['ticker', 'date', 'open', 'high', 'low', 'close', 
+                    'O-to-C', 'OC-20D-Mean', 'volume', 'Volume-20D-Mean', 
+                    'MaxOC_Prev10', 'SellingPressure', 'OC-%-from-20D-Mean', 
+                    'Volume-%-from-20D-Mean', 
+                    ]]
+    # Select the subset of dataframe where breakout conditions apply
+    # Conditions: 1. green candle, 2. candle's body is longest in 10 days, 
+    # 3. breakout volume is 50% higher than the rolling 20-day average, and
+    # 4. breakout candle has body that is 100% higher than the rolling 20-day average
+    condition = (prices['O-to-C'] > 0.0) & (prices['O-to-C'] == prices['MaxOC_Prev10']) & (prices['OC-%-from-20D-Mean'] >= 100.0) & (prices['SellingPressure']/prices['O-to-C'] <= 0.40) & (prices['Volume-%-from-20D-Mean'] >= 50.0)
+    breakouts = prices[condition]
+    ## GET THE PROFIT (OR LOSS) PER CANDLE
+    # Get the index (from the dataframe) of each breakout row, which is necessary for looping later
+    breakouts_indices = breakouts.index.tolist()
     profits = []
     try:
         for index in breakouts_indices:
@@ -79,13 +77,13 @@ def breakout_profitability(ticker, dollars):
                 if highest_price_index < lowest_price_index:
                     profit = round(100*(highest_price_within10days - breakout_close)/breakout_close, 2)
                     profits.append(profit)
-                elif lowest_price_index <= highest_price_index:
-                    profit = round(100*(breakout_open - breakout_close)/breakout_close, 2)
-                    profits.append(profit)
+            elif lowest_price_index <= highest_price_index:
+                profit = round(100*(breakout_open - breakout_close)/breakout_close, 2)
+                profits.append(profit)
             else:
                 profit = round(100*(highest_price_within10days - breakout_close)/breakout_close, 2)
                 profits.append(profit)
-    except (UnboundLocalError, IndexError):
+    except IndexError:
         pass
     ## GET PROFIT PER TYPE TO CALCULATE SOME PROBABILITIES
     wins = []
@@ -113,19 +111,13 @@ def breakout_profitability(ticker, dollars):
     # Supply other information, in addition to the chart, for the output
     # NOTE: breakout_dates are in timestamp, so we have to convert to date format
     try:
-        today_date = pd.to_datetime('today').date()
+        curr_day = datetime.today().date()
         breakout_dates = pd.to_datetime(breakouts['date'])
         earliest_breakout = breakout_dates.min().strftime('%Y-%m-%d')
         latest_breaktout = breakout_dates.max().strftime('%Y-%m-%d')
-    except (ValueError, UnboundLocalError):
-        pass
-    try:
-        counter = 0
-        if latest_breaktout == today_date:
-            supplementary_info = f"Additional Info: The first breakout for {ticker} was observed on {earliest_breakout} while the most recent breakout was on {latest_breaktout}. The holding period for each breakout trade is maximum of 10 days."        
+        supplementary_info = f"Additional Info: The first breakout for {ticker} was observed on {earliest_breakout} while the most recent breakout was on {latest_breaktout}. The holding period for each breakout trade is maximum of 10 days. The EV is {ev}%"
+        if breakout_dates == curr_day:
             print(supplementary_info)
-            print(ev)
-            counter += 1
             # # VISUALIZE DISTRIBUTION OF PROFITS
             # sns.histplot(pd.Series(profits), bins=20)
             # plt.title(f"Distribution of Breakout Profits for {ticker.upper()}")
@@ -134,7 +126,5 @@ def breakout_profitability(ticker, dollars):
             # plt.ylabel('Number of Breakouts')
             # plt.xlabel('Profit (%)')
             # plt.show()
-        if counter == 0:
-            print("No Stocks have a breakout today.")
-    except UnboundLocalError:
+    except (ValueError, UnboundLocalError):
         pass
