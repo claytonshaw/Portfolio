757902324169a65703d83951fa857848d5609e62
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-11-21 08:32:51 -0700

    Update functions.py

--- Stock Breakout/functions.py
+++ Stock Breakout/functions.py
@@ -16,42 +16,42 @@ def breakout_profitability(ticker, dollars):
     try:
         # Get the historical weekly prices from the specified start date and end date (both YYYY-mm-dd)
         hist = get_data(ticker, index_as_date=False)
-    except AssertionError:
-        pass
-    # Drop the adjusted close column
-    prices = hist.drop(['adjclose'], axis=1)
-    # Get the selling pressure, which is distance between high and close
-    prices['SellingPressure'] = prices['high'] - prices['close'] 
-    # Get the length of candle's body (from open to close)
-    prices['O-to-C'] = prices['close'] - prices['open']
-    # Get the rolling mean of the candle's body for recent 20 candles
-    prices['OC-20D-Mean'] = prices['O-to-C'].rolling(20).mean()
-    # Get the % increase or decrease of the current body length from the rolling mean
-    prices['OC-%-from-20D-Mean'] = 100*(prices['O-to-C'] - prices['OC-20D-Mean'])/prices['OC-20D-Mean']
-    # Get the maximum OC compared to the recent 10 candles
-    prices['MaxOC_Prev10'] = prices['O-to-C'].rolling(10).max()
-    # Get the rolling mean of volume for the recent 20 candles
-    prices['Volume-20D-Mean'] = prices['volume'].rolling(20).mean()
-    # Get the % increase or decrease of the current volume from the rolling mean
-    prices['Volume-%-from-20D-Mean'] = 100*(prices['volume'] - prices['Volume-20D-Mean'])/prices['Volume-20D-Mean']
-    # Drop the null values for the first 19 rows, where no mean can be computed yet.
-    prices = prices.dropna()
-    # Rearrange columns
-    prices = prices[['ticker', 'date', 'open', 'high', 'low', 'close', 
+        # Drop the adjusted close column
+        prices = hist.drop(['adjclose'], axis=1)
+        # Get the selling pressure, which is distance between high and close
+        prices['SellingPressure'] = prices['high'] - prices['close'] 
+        # Get the length of candle's body (from open to close)
+        prices['O-to-C'] = prices['close'] - prices['open']
+        # Get the rolling mean of the candle's body for recent 20 candles
+        prices['OC-20D-Mean'] = prices['O-to-C'].rolling(20).mean()
+        # Get the % increase or decrease of the current body length from the rolling mean
+        prices['OC-%-from-20D-Mean'] = 100*(prices['O-to-C'] - prices['OC-20D-Mean'])/prices['OC-20D-Mean']
+        # Get the maximum OC compared to the recent 10 candles
+        prices['MaxOC_Prev10'] = prices['O-to-C'].rolling(10).max()
+        # Get the rolling mean of volume for the recent 20 candles
+        prices['Volume-20D-Mean'] = prices['volume'].rolling(20).mean()
+        # Get the % increase or decrease of the current volume from the rolling mean
+        prices['Volume-%-from-20D-Mean'] = 100*(prices['volume'] - prices['Volume-20D-Mean'])/prices['Volume-20D-Mean']
+        # Drop the null values for the first 19 rows, where no mean can be computed yet.
+        prices = prices.dropna()
+        # Rearrange columns
+        prices = prices[['ticker', 'date', 'open', 'high', 'low', 'close', 
                     'O-to-C', 'OC-20D-Mean', 'volume', 'Volume-20D-Mean', 
                     'MaxOC_Prev10', 'SellingPressure', 'OC-%-from-20D-Mean', 
                     'Volume-%-from-20D-Mean', 
                     ]]
-    # Select the subset of dataframe where breakout conditions apply
-    # Conditions: 1. green candle, 2. candle's body is longest in 10 days, 
-    # 3. breakout volume is 50% higher than the rolling 20-day average, and
-    # 4. breakout candle has body that is 100% higher than the rolling 20-day average
-    condition = (prices['O-to-C'] > 0.0) & (prices['O-to-C'] == prices['MaxOC_Prev10']) & (prices['OC-%-from-20D-Mean'] >= 100.0) & (prices['SellingPressure']/prices['O-to-C'] <= 0.40) & (prices['Volume-%-from-20D-Mean'] >= 50.0)
-    breakouts = prices[condition]
-    ## GET THE PROFIT (OR LOSS) PER CANDLE
-    # Get the index (from the dataframe) of each breakout row, which is necessary for looping later
-    breakouts_indices = breakouts.index.tolist()
-    profits = []
+        # Select the subset of dataframe where breakout conditions apply
+        # Conditions: 1. green candle, 2. candle's body is longest in 10 days, 
+        # 3. breakout volume is 50% higher than the rolling 20-day average, and
+        # 4. breakout candle has body that is 100% higher than the rolling 20-day average
+        condition = (prices['O-to-C'] > 0.0) & (prices['O-to-C'] == prices['MaxOC_Prev10']) & (prices['OC-%-from-20D-Mean'] >= 100.0) & (prices['SellingPressure']/prices['O-to-C'] <= 0.40) & (prices['Volume-%-from-20D-Mean'] >= 50.0)
+        breakouts = prices[condition]
+        ## GET THE PROFIT (OR LOSS) PER CANDLE
+        # Get the index (from the dataframe) of each breakout row, which is necessary for looping later
+        breakouts_indices = breakouts.index.tolist()
+        profits = []
+    except (AssertionError, KeyError):
+        pass
     try:
         for index in breakouts_indices:
             # For a given breakout candle index, slice the historical prices dataframe 10 rows RIGHT AFTER the breakout row
@@ -83,19 +83,22 @@ def breakout_profitability(ticker, dollars):
             else:
                 profit = round(100*(highest_price_within10days - breakout_close)/breakout_close, 2)
                 profits.append(profit)
-    except IndexError:
+    except (IndexError, UnboundLocalError):
         pass
     ## GET PROFIT PER TYPE TO CALCULATE SOME PROBABILITIES
-    wins = []
-    breakevens = []
-    losses = []
-    for profit in profits:
-        if profit > 0.0:
-            wins.append(profit)
-        elif profit == 0.0:
-            breakevens.append(profit)
-        elif profit < 0.0:
-            losses.append(profit)
+    try:
+        wins = []
+        breakevens = []
+        losses = []
+        for profit in profits:
+            if profit > 0.0:
+                wins.append(profit)
+            elif profit == 0.0:
+                breakevens.append(profit)
+            elif profit < 0.0:
+                losses.append(profit)
+    except UnboundLocalError:
+        pass
     # Calculate some trading probabilities
     try:
         win_rate = round(100*len(wins)/len(profits), 2)
@@ -106,7 +109,7 @@ def breakout_profitability(ticker, dollars):
         num1 = (ave_positive_profit/100) * dollars * (win_rate/100)
         num2 = abs((ave_negative_profit/100) * dollars * (loss_rate/100))
         ev = round(((num1 - num2)/dollars) * 100, 2)
-    except ZeroDivisionError:
+    except (ZeroDivisionError, UnboundLocalError):
         pass
     # Supply other information, in addition to the chart, for the output
     # NOTE: breakout_dates are in timestamp, so we have to convert to date format
@@ -118,14 +121,14 @@ def breakout_profitability(ticker, dollars):
         supplementary_info = f"Additional Info: The first breakout for {ticker} was observed on {earliest_breakout} while the most recent breakout was on {latest_breaktout}. The holding period for each breakout trade is maximum of 10 days. The EV is {ev}%"
         if breakout_dates == curr_day:
             print(supplementary_info)
-            # # VISUALIZE DISTRIBUTION OF PROFITS
-            # sns.histplot(pd.Series(profits), bins=20)
-            # plt.title(f"Distribution of Breakout Profits for {ticker.upper()}")
-            # plt.text(0.95, 0.95, f"Total Breakouts: {len(profits)} \n Ave. Positive Profit: {ave_positive_profit}% \n Ave. Negative Profit: {ave_negative_profit}% \n Win Rate: {win_rate}% \n Loss Rate: {loss_rate}% \n Breakeven Rate: {breakeven_rate}% \n EV: {ev}%", 
-            #         ha='right', va='top', transform=plt.gca().transAxes)
-            # plt.ylabel('Number of Breakouts')
-            # plt.xlabel('Profit (%)')
-            # plt.show()
+            # VISUALIZE DISTRIBUTION OF PROFITS
+            sns.histplot(pd.Series(profits), bins=20)
+            plt.title(f"Distribution of Breakout Profits for {ticker.upper()}")
+            plt.text(0.95, 0.95, f"Total Breakouts: {len(profits)} \n Ave. Positive Profit: {ave_positive_profit}% \n Ave. Negative Profit: {ave_negative_profit}% \n Win Rate: {win_rate}% \n Loss Rate: {loss_rate}% \n Breakeven Rate: {breakeven_rate}% \n EV: {ev}%", 
+                    ha='right', va='top', transform=plt.gca().transAxes)
+            plt.ylabel('Number of Breakouts')
+            plt.xlabel('Profit (%)')
+            plt.show()
         else:
             pass
     except (ValueError, UnboundLocalError):
