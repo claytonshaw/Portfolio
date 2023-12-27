32ba4d44890e4e5be30e8a25e6e88efeb2a36fb5
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2023-12-26 22:56:35 -0700

    Update stock_breakout_df.py

--- Stock Breakout/stock_breakout_df.py
+++ Stock Breakout/stock_breakout_df.py
@@ -49,13 +49,13 @@ results = {}
 # list of parameters
 account_value = 3238.29 # updated 12/16/2023
 dollars_to_trade = account_value / 10 # seperate the account value into 10 equal portions because we have a 10 day holding period
-target_ev = 10
+target_ev = 2
 
 # loop through each stock to get info
 for stock in sav_set:
     try:
         stock_info = {}
-        eb, wr, br, lr, app, anp, ev, tb = breakout_profitability(stock, dollars_to_trade, target_ev, days_back)
+        eb, wr, br, lr, app, anp, ev, tb = breakout_profitability(stock, dollars_to_trade, ev=target_ev, days_back=days_back)
         stock_info['earliest_breakout'] = eb
         stock_info['total_breakouts'] = tb
         stock_info['breakeven_rate'] = f'{br}%'
@@ -75,4 +75,4 @@ number_of_stocks = len(results)
 
 # printing info to use when making the trades
 print("Number of Stocks to Trade:", number_of_stocks)
-print("Max available per trade:", f"${round(dollars_to_trade/number_of_stocks, ndigits=2)}") 
+print("Amount to use on each trade:", f"${dollars_to_trade/number_of_stocks}")
